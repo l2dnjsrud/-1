51647ed57a9e3c4167cb505055ed174aa4ec5e43
Wonkyung Lee <69292312+l2dnjsrud@users.noreply.github.com>
2022-01-18 17:31:00 +0900

    Delete ch04 directory

--- ch04/cross_entropy_error_numlabel.py
+++
@@ -1,7 +0,0 @@
-def cross_entropy_error(y, t):
-    if y.ndim == 1:     #y가 1차원, 즉 데이터 하나
-        t = t.reshape(1, t.size)
-        y = y.reshape(1, y.size)
-
-    batch_size = y.shape[0]
-    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size   #각 데이터의 정답 레이블에 해당하는 신경망 출력을 추출
--- ch04/sum_squares_error.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-import numpy as np
-def sum_squares_error(y, t):
-    return 0.5 * np.sum((y - t)**2)
-
-t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]      #정답이 2인 경우
-y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]     # 예1 : '2'일 확률이 가장 높다고 추청(0.6)
-sum_squares_error(np.array(y), np.array(t))
