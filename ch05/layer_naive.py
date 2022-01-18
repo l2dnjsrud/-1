fe56867d38748db04657b4df45e38352cb20daf8
Wonkyung Lee <hjtwin2@gmail.com>
2022-01-18 18:53:06 +0900

    Update layer_naive.py

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -18,4 +18,16 @@ class MulLayer:
         dy = dout * self.x
 
         return dx, dy
-        
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backward(self):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy       
