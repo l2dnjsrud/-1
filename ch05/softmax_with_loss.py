f439422eeef8f9601b0648fd0a9ccf5f04a02fbd
Wonkyung Lee <hjtwin2@gmail.com>
2022-01-19 14:36:18 +0900

    Create softmax_with_loss.py

---
+++ ch05/softmax_with_loss.py
@@ -0,0 +1,22 @@
+import sys, os
+sys.path.append(os.pardir)
+from common.functions import cross_entropy_error, softmax
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        slef.loss = None  # 손실
+        self.y = None  #softmax의 출력
+        self.t = None  #정답 레이블(원-핫 백터)
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout = 1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx 
