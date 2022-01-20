ae230dbd207bd68920eefb622f7919c57a5b5329
Wonkyung Lee <hjtwin2@gmail.com>
2022-01-20 15:32:14 +0900

    Create using_im2col.py

---
+++ ch07/using_im2col.py
@@ -0,0 +1,12 @@
+import sys, os
+sys.path.append(os.pardir)
+from common.util import im2col
+import numpy as np
+
+x1 = np.random.rand(1, 3, 7, 7)  #(데이터 수, 채널 수, 높이, 너비)
+col1 = im2col(x1, 5, 5, stride=1, pad=0)
+print(col1.shape)
+
+x2 = np.random.rand(10, 3, 7, 7)  #데이터 10개
+col2 = im2col(x2, 5, 5, stride=1, pad=0)
+print(col2.shape)
